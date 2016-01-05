c771c3b92c8f41aa58b314c0452ab080b4d9e4f8
sirMackk <c.tairix@gmail.com>
2016-01-05 18:45:13 +0100

    Starts minor cleanup

--- lib/stockfighter/cli.ex
+++ lib/stockfighter/cli.ex
@@ -1,9 +1,4 @@
 defmodule Stockfigher.CLI do
-  @api_key Application.get_env(:stockfighter, :api_key)
-  @api_url Application.get_env(:stockfighter, :api_url)
-  @wss_url "wss://api.stockfighter.io/ob/api/ws"
-  @headers [{"User-agent", "pr0tagon1st/stockfighter/elixirlang"},
-            {"X-Starfighter-Authorization", @api_key}]
 
   def main(args) do
     args
@@ -11,73 +6,10 @@ defmodule Stockfigher.CLI do
       |> process
   end
 
-  defp venue_stock_url(venue) do
-    @api_url <> "/venues/#{venue}/stocks"
-  end
-
-  defp orderbook_url(venue, stock) do
-    @api_url <> "/venues/#{venue}/stocks/#{stock}"
-  end
-
-  defp new_order_url(venue, stock) do
-    @api_url <> "/venues/#{venue}/stocks/#{stock}/orders"
-  end
-
-  defp tickertape_url(account, venue) do
-    @wss_url <> "/#{account}/venues/#{venue}/tickertape"
-  end
-
-  defp stock_tickertape(account, venue, stock) do
-    @wss_url <> "/#{account}/venues/#{venue}/tickertape/stocks/#{stock}"
-  end
-
-  def get_stocks_for(venue) do
-    venue_stock_url(venue)
-      |> HTTPoison.get(@headers)
-      |> handle_get_response
-  end
-
-  def get_order_book_stock(venue, stock) do
-    orderbook_url(venue, stock)
-      |> HTTPoison.get(@headers)
-      |> handle_get_response
-  end
-
-  def buy_stock(venue, stock, qty, price, acc) do
-    post_new_order(venue, stock, qty, price, acc, "buy")
-  end
-
-  def sell_stock(venue, stock, qty, price, acc) do
-    post_new_order(venue, stock, qty, price, acc, "sell")
-  end
-
-  defp post_new_order(venue, stock, qty, price, acc, direction) do
-    new_order_url(venue, stock)
-      |> HTTPoison.post(@headers)
-      |> handle_post_response
-  end
 
 
-  # move above out of this module, this should only have top level cli cmds
   # venue_setup =
   # get stocks on venue, get orderbooks for each stock (or specific stock)
-  # MAKE STOCK ORDERBOOK an AGENT
-  # get tickertape updates to stock
   # execute actions - actions - actions happen on conditions when something about stock or time happens
 end
 
-defmodule Stockfighter.Orderbook do
-  # how can this be a time-series map?
-  def start_link(venue) do
-    Agent.start_link(fn -> HashDict.new end, name: venue)
-  end
-
-  def get_price(venue, stock) do
-    Agent.get(venue, &Dict.fetch(&1, stock))
-  end
-  def update(stock, price) do
-    Agent.update(venue, fn (dict) -> 
-      Dict.put(dict, stock, price)
-    end)
-  end
-end
---
+++ lib/stockfighter/orderbook.ex
@@ -0,0 +1,15 @@
+defmodule Stockfighter.Orderbook do
+  # how can this be a time-series map?
+  def start_link(venue) do
+    Agent.start_link(fn -> HashDict.new end, name: venue)
+  end
+
+  def get_price(venue, stock) do
+    Agent.get(venue, &Dict.fetch(&1, stock))
+  end
+  def update(stock, price) do
+    Agent.update(venue, fn (dict) -> 
+      Dict.put(dict, stock, price)
+    end)
+  end
+end
